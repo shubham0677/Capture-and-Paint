b1915ed73fe1ab83cebaa48109da8409be8c57cc
shubham0677 <shubham.sharma0677@gmail.com>
2017-07-26 23:57:26 -0400

    Added methods to check for storage permission

--- app/src/main/java/com/sharma/shubham/captureandpaint/MainActivity.java
+++ app/src/main/java/com/sharma/shubham/captureandpaint/MainActivity.java
@@ -27,16 +27,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//      public native String stringFromJNI();
+//
+//    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
 
     /***
      * On click method for capture image button
